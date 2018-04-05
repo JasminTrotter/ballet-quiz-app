3b975e398bb8a7e1d645a8d12bef612e9383733d
Jasmin Trotter <jasmintrotter@Jasmins-iMac.local>
2018-04-05 12:29:10 -0700

    first commit

---
+++ datastore.js
@@ -0,0 +1,102 @@
+const STORE = [
+  {
+    question: 'From which country did ballet originate?',
+    answers: [
+      'Russia',
+      'France',
+      'England',
+      'Italy'
+      ],
+      correctAnswer: 'Italy'
+  },
+  {
+    question: 'Who introduced ballet to France?',
+    answers: [
+      'Pyotr Ilyich Tchaikovsky',
+      'Leonardo DaVinci',
+      'Catherine de Medici',
+      'George Balanchine'
+      ],
+      correctAnswer: 'Catherine de Medici'
+  },
+  {
+    question: 'Which French king danced in ballet performances?',
+    answers: [
+      'Francis I',
+      'Henry IV',
+      'Louis XIV',
+      'Louis XVI'
+      ],
+    correctAnswer: 'Louis XIV'
+  },
+  {
+    question: 'What is a ballerina?',
+    answers: [
+      'A female who dances for a professional ballet company',
+      'A female dancer of the highest rank in a ballet company',
+      'A female dancer who has trained in ballet for at least 5 years',
+      'A female dancer who performs ballet on stage'
+      ],
+    correctAnswer: 'A female dancer of the highest rank in a ballet company',
+  },
+  {
+    question: 'What is a male ballet dancer called?',
+    answers: [
+      'Ballerino',
+      'Danseur',
+      'Balletomane',
+      'Allegro'
+      ],
+    correctAnswer: 'Danseur'
+  },
+  {
+    question: 'What is is the technical term for dancing on the tips of the toes?',
+    answers: [
+      'on point',
+      'pointilism',
+      'pointing the toes',
+      'en pointe', 
+      ],
+    correctAnswer: 'en pointe'
+  },
+  {
+    question: 'What are pointe shoes typically made out of?',
+    answers: [
+      'layers of fabric | glue | satin | hard leather',
+      'layers of fabric | clay | satin | particle board',
+      'layers of fabric | glue | silk | wood',
+      'layers of fabric | cement | silk | plastic'
+      ],
+    correctAnswer: 'layers of fabric | glue | satin | hard leather'
+  },
+  {
+    question: 'What is the minimum age a student must be before they start training en pointe?',
+    answers: [
+      '7 years old',
+      '10 years old',
+      '12 years old',
+      '15 years old'
+      ],
+    correctAnswer: '12 years old'
+  },
+  {
+    question: 'How long does it typically take to become a professional ballet dancer?',
+    answers: [
+      '1-2 years',
+      '3-5 years',
+      '6-7 years',
+      '8-10 years'
+      ],
+    correctAnswer: '8-10 years'
+  },
+  {
+    question: 'Which of the following is NOT a ballet movement?',
+    answers: [
+      'sashé', 
+      'lame duck', 
+      'port de bras', 
+      'balonné' 
+      ],
+    correctAnswer: 'sashé'
+  }
+];
